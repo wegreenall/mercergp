e6d5796733d1453b0a3ef4481e8b8cbc9de45a28
wegreenall <wjgreenall@gmail.com>
2023-01-03 00:00:57 +0200

    added initial tests for FourierGPPosterior

--- test/test_mercer_gaussian_processes.py
+++ test/test_mercer_gaussian_processes.py
@@ -6,10 +6,19 @@ from ortho.basis_functions import (
     smooth_exponential_basis_fasshauer,
     smooth_exponential_eigenvalues_fasshauer,
     Basis,
+    RandomFourierFeatureBasis,
 )
 
 from mercergp.kernels import MercerKernel
-from mercergp.MGP import MercerGP, RFFGP
+from mercergp.MGP import (
+    MercerGP,
+    RFFGP,
+    NonStationaryRFFGP,
+    HilbertSpaceElement,
+    MercerGPFourierPosterior,
+    PosteriorGPSample,
+)
+from mercergp.posterior_sampling import MercerSpectralDistribution
 
 
 def output_function(x: torch.Tensor) -> torch.Tensor:
@@ -90,7 +99,9 @@ class TestMercerGP(unittest.TestCase):
 
         self.mercer_gp.add_data(test_inputs, test_outputs)
 
-        coefficients = self.mercer_gp._calculate_posterior_coefficients()
+        coefficients = self.mercer_gp._calculate_posterior_coefficients(
+            test_inputs, test_outputs
+        )
         self.assertEqual(coefficients.shape, torch.Size([self.order]))
         return
 
@@ -100,7 +111,9 @@ class TestMercerGP(unittest.TestCase):
 
         self.mercer_gp.add_data(test_inputs, test_outputs)
 
-        coefficients = self.mercer_gp._calculate_posterior_coefficients()
+        coefficients = self.mercer_gp._calculate_posterior_coefficients(
+            test_inputs, test_outputs
+        )
         self.assertEqual(coefficients.shape, torch.Size([self.order]))
         return
 
@@ -166,13 +179,13 @@ class TestMercerGP(unittest.TestCase):
         self.assertTrue(torch.allclose(probs_tensor, potential_values))
 
 
-@unittest.skip("Not correct")
+# @unittest.skip("Not correct")
 class TestRFFMercerGP(unittest.TestCase):
     def setUp(self):
         # basis parameters
         # self.basis_function = smooth_exponential_basis_fasshauer
         self.dimension = 1
-        self.order = 10
+        self.order = 1000
 
         # parameters
         l_se = torch.Tensor([[5]])
@@ -188,25 +201,28 @@ class TestRFFMercerGP(unittest.TestCase):
 
         self.params = se_kernel_args
 
-        self.spectral_distribution = D.Normal(0.0, 1.0)
+        self.spectral_distribution = D.Normal(
+            torch.Tensor([0.0]), torch.Tensor([1.0])
+        )
 
         # gp parameters
-        self.mercer_gp = RFFGP(
+        self.rff_gp = RFFGP(
             self.order,
             self.dimension,
             self.spectral_distribution,
         )
         return
 
+    @unittest.skip("")
     def test_adding_data(self):
-        x = torch.Tensor([0.1, 4, 5.24, 7])
-        x2 = torch.Tensor([2, 3, 4, 5])
-        y = torch.Tensor([3, 4, 5, 6])
-        y2 = torch.Tensor([9, 2, 7, 8])
+        x = torch.Tensor([0.1, 4, 5.24, 7]).t()
+        x2 = torch.Tensor([2, 3, 4, 5]).t()
+        y = torch.Tensor([3, 4, 5, 6]).t()
+        y2 = torch.Tensor([9, 2, 7, 8]).t()
 
-        self.mercer_gp.add_data(x, y)
-        self.mercer_gp.add_data(x2, y2)
-        xs, ys = self.mercer_gp.get_inputs(), self.mercer_gp.get_outputs()
+        self.rff_gp.add_data(x, y)
+        self.rff_gp.add_data(x2, y2)
+        xs, ys = self.rff_gp.get_inputs(), self.rff_gp.get_outputs()
 
         self.assertTrue(
             (xs == torch.Tensor([0.1, 4, 5.24, 7, 2, 3, 4, 5])).all()
@@ -218,9 +234,11 @@ class TestRFFMercerGP(unittest.TestCase):
         test_inputs = torch.linspace(0, 1, 20)
         test_outputs = torch.linspace(0, 1, 20)
 
-        self.mercer_gp.add_data(test_inputs, test_outputs)
+        self.rff_gp.add_data(test_inputs, test_outputs)
 
-        coefficients = self.mercer_gp._calculate_posterior_coefficients()
+        coefficients = self.rff_gp._calculate_posterior_coefficients(
+            test_inputs, test_outputs
+        )
         self.assertEqual(coefficients.shape, torch.Size([self.order]))
         return
 
@@ -229,11 +247,92 @@ class TestRFFMercerGP(unittest.TestCase):
         test_inputs = torch.linspace(0, 1, 20).unsqueeze(1)
         test_outputs = torch.linspace(0, 1, 20).unsqueeze(1)
 
-        self.mercer_gp.add_data(test_inputs, test_outputs)
+        self.rff_gp.add_data(test_inputs, test_outputs)
 
-        coefficients = self.mercer_gp._calculate_posterior_coefficients()
+        coefficients = self.rff_gp._calculate_posterior_coefficients(
+            test_inputs, test_outputs
+        )
         self.assertEqual(coefficients.shape, torch.Size([self.order]))
         return
+
+
+class TestMercerFourierGPPosterior(unittest.TestCase):
+    def setUp(self):
+        # hyperparameters and basis counts
+        self.order = 15
+        self.rff_order = 5000
+        self.frequency = 1000
+        self.dimension = 1
+        self.kernel_args = {
+            "ard_parameter": torch.Tensor([[1.0]]),
+            "noise_parameter": torch.Tensor([0.1]),
+            "precision_parameter": torch.Tensor([1.0]),
+        }
+        self.eigenvalues = smooth_exponential_eigenvalues_fasshauer(
+            self.order, self.kernel_args
+        )
+        # MercerGPFourierPosterior parameters
+        self.spectral_distribution = MercerSpectralDistribution(
+            self.frequency, torch.ones(self.rff_order)
+        )
+
+        self.basis = Basis(
+            smooth_exponential_basis_fasshauer, 1, self.order, self.kernel_args
+        )
+        self.rff_basis = RandomFourierFeatureBasis(
+            self.dimension, self.order, self.spectral_distribution
+        )
+        self.dimension = 1
+        self.kernel = MercerKernel(
+            self.order, self.basis, self.eigenvalues, self.kernel_args
+        )
+
+        # GP setup
+        self.mercer_gp = MercerGPFourierPosterior(
+            self.basis,
+            self.rff_basis,
+            self.order,
+            self.rff_order,
+            self.dimension,
+            self.kernel,
+        )
+        pass
+
+    def test_gen_gp(self):
+        gp = self.mercer_gp.gen_gp()
+        self.assertTrue(isinstance(gp, PosteriorGPSample))
+        pass
+
+
+class TestNonStationaryRFFGP(unittest.TestCase):
+    def setUp(self):
+        self.frequency = 1000
+        self.order = 5000
+        self.dimension = 1
+
+        self.spectral_distribution = MercerSpectralDistribution(
+            self.frequency, torch.ones(self.order)
+        )
+        basis = RandomFourierFeatureBasis(
+            self.dimension, self.order, self.spectral_distribution
+        )
+        self.nsrffgp = NonStationaryRFFGP(basis, self.order, self.dimension)
+        pass
+
+    def test_get_sample_coefficients(self):
+        sample_coeffics = self.nsrffgp._get_sample_coefficients()
+        self.assertEqual(sample_coeffics.shape, torch.Size([self.order]))
+
+    def test_get_gp_size(self):
+        sample_gp = self.nsrffgp.gen_gp()
+        self.assertEqual(sample_gp.get_order(), self.order)
+
+    def test_get_gp(self):
+        sample_gp = self.nsrffgp.gen_gp()
+        self.assertTrue(
+            isinstance(sample_gp, HilbertSpaceElement),
+            2 * self.order,
+        )
 
 
 if __name__ == "__main__":
