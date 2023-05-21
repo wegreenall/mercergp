Make all tests pass! it appears the the likelihood is now producing the same (correct) gradient as before, ostensibly with proper speed.

likelihood_refit.py
@@ -121,7 +121,7 @@ class TermGenerator:
         data_term = ((intermediate_term @ self.phi_y) ** 2).squeeze()
 
         trace_term = torch.diag(intermediate_term @ self.phi_phi).squeeze()
-        return data_term - trace_term
+        return 0.5 * (data_term - trace_term)
 
 
 class Likelihood:
@@ -167,9 +167,9 @@ class Likelihood:
 
         # convergence criterion
         self.epsilon = optimisation_threshold
-
-        # memoisation stuff
-        self.phi_matrix = self.kernel.basis(self.input_sample)
+        self.term_generator = TermGenerator(
+            input_sample, output_sample, kernel
+        )
 
     def fit(
         self,
@@ -231,13 +231,6 @@ class Likelihood:
 
             if verbose:
                 pass
-                # tabulated_data = get_tabulated_data(
-                # trained_noise,
-                # trained_parameters,
-                # noise_gradient,
-                # parameters_gradients,
-                # )
-                # print(tabulated_data)
             # having updated parameters and noise values, change on the kernel
             self.update_kernel_parameters(trained_parameters, trained_noise)
 
@@ -311,18 +304,15 @@ class Likelihood:
             sigma_grad: [1]
             params_grad: [b x 1]
         """
-        # precalculates the kernel inverse for speed
-
-        kernel_inverse: torch.Tensor = self.kernel.kernel_inverse(
-            self.input_sample
-        )
+        # term generator
+        eigenvalues = self.eigenvalue_generator(parameters)
+        vector_term = self.term_generator.get_vector_term(eigenvalues, noise)
 
         # get the terms
-        noise_gradient: torch.Tensor = self.noise_gradient(
-            kernel_inverse, parameters, noise
-        )
+        # the noise gradient if you do it the term_generator way
+        noise_gradient: torch.Tensor = 2 * noise * torch.sum(vector_term)
         parameters_gradients: dict = self.parameters_gradient(
-            kernel_inverse, parameters
+            vector_term, parameters
         )
         return noise_gradient, parameters_gradients
 
@@ -366,10 +356,14 @@ class Likelihood:
         )
         trace_term = 0.5 * torch.trace(kernel_inverse @ sigma_gradient_term)
 
-        return data_term - trace_term
+        return data_term - trace_term  # the whole noise gradient
 
     def parameters_gradient(
-        self, kernel_inverse: torch.Tensor, parameters: dict
+        self,
+        term_vector: torch.Tensor,
+        parameters: dict,
+        # trained_noise: torch.Tensor,
+        # eigenvalues: torch.Tensor,
     ) -> dict:
         """
         Returns the gradient of the negative log likelihood w.r.t the
@@ -413,29 +407,17 @@ class Likelihood:
             parameters
         )  # the dictionary
 
-        # get the terms
+        # For each of the parameters, take the gradient given by the eigenvalue
+        # generator and get the vector of values from the term_generator.
+        # Then, for each of the parameters, inner-product the term generator
+        # vector
         for param in parameters:
-            self.kernel.set_eigenvalues(eigenvalue_derivatives[param])
-            kernel_gradient_term: dict = self.kernel(
-                self.input_sample, self.input_sample
+            # get the vector of eigenvalue derivatives for this parameter
+            eigenvalue_derivative_vector = eigenvalue_derivatives[param]
+            # breakpoint()
+            parameter_gradients[param] = (
+                term_vector @ eigenvalue_derivative_vector
             )
-            # calculate the gradient
-            data_term = 0.5 * torch.einsum(
-                "i, ij, jk, kl, l -> ",
-                self.output_sample,  # i
-                kernel_inverse,  # ij
-                kernel_gradient_term,  # jk
-                kernel_inverse,  # kl
-                self.output_sample,  # l
-            )
-
-            trace_term = 0.5 * torch.trace(
-                kernel_inverse @ kernel_gradient_term
-            )
-            parameter_gradients[param] = data_term - trace_term
-            # WARNING: EXPERIMENTATION ONLY:
-            # print("ARE YOU TESTING? IF NOT THIS IS WRONG!")
-            # parameter_gradients[param] = data_term  # - trace_term
 
         return parameter_gradients
 

test/test_likelihood_refit.py
@@ -104,9 +104,16 @@ class TestLikelihoodRefit(unittest.TestCase):
 
         If they are the same, replace the likelihood method.
         """
+        true_vector_term = self.term_generator.get_vector_term(
+            self.eigenvalues, self.noise
+        )
         true_term = self.likelihood.parameters_gradient(
-            self.kernel_inverse, self.parameters
+            true_vector_term, self.parameters
         )["ard_parameter"]
+        _, true_term = self.likelihood.get_gradients(
+            self.parameters, self.noise
+        )
+        true_term = true_term["ard_parameter"]
 
         # get the derivative kernel matrix term: δK/δθ
         # true terms
@@ -150,6 +157,7 @@ class TestLikelihoodRefit(unittest.TestCase):
         )
         self.assertTrue(torch.allclose(data_term, test_data_term_2))
 
+        # breakpoint()
         # # now do it by hand
         phikyvector = self.PhiKyVector()
         phikyvectorsquared = self.PhiKyVector() ** 2
@@ -174,11 +182,12 @@ class TestLikelihoodRefit(unittest.TestCase):
             @ self.eigenvalue_derivatives
         )
 
+        # breakpoint()
         # vector term from term generator
         vector_term = self.term_generator.get_vector_term(
             self.eigenvalues, self.noise
         )
-        test_data_term_final = 0.5 * vector_term @ self.eigenvalue_derivatives
+        test_data_term_final = vector_term @ self.eigenvalue_derivatives
 
         # true term is the full thing with trace term subtracted etc.
         self.assertTrue(
@@ -187,7 +196,9 @@ class TestLikelihoodRefit(unittest.TestCase):
                 test_data_term_final,
             )
         )
-        breakpoint()
+
+        # compare the results from the true likelihood, with the results
+        # from the term generator
         self.assertTrue(torch.allclose(true_term, test_data_term_final))
 
     def PhiKyVector(self):
@@ -196,6 +207,16 @@ class TestLikelihoodRefit(unittest.TestCase):
     def PhiKPhiVector(self):
         return torch.diag(
             self.phi_matrix.T @ self.kernel_inverse @ self.phi_matrix
+        )
+
+    def test_noise_gradient(self):
+        # get the noise gradient from the likelihood
+        # compare it to that build from the term_vector stuff
+        true_term = self.likelihood.noise_gradient(
+            self.kernel_inverse, self.parameters, self.noise
+        )
+        term_generator_version, _ = self.likelihood.get_gradients(
+            self.parameters, self.noise
         )
 
 
@@ -294,7 +315,8 @@ class TestTermGenerator(unittest.TestCase):
         )
         self.assertTrue(
             torch.allclose(
-                vector_term, handmade_vector_term_1 - handmade_vector_term_2
+                vector_term,
+                0.5 * (handmade_vector_term_1 - handmade_vector_term_2),
             )
         )
 